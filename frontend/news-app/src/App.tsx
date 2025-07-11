bcbf35ef03187983c3ea3bd02cf4f42f87a57890
Poorna <poornarajapaksha339@gamil.com>
2025-07-12 01:50:01 +0530

    Implement news fetching and display in App component

--- frontend/news-app/src/App.tsx
+++ frontend/news-app/src/App.tsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
+import NewsCard from "./components/NewsCard";
+
 function App() {
-  return (
-    <>
-      <p>Hi Mom</p>
-    </>
-  );
+  const API_URL = "http://127.0.0.1:8000/";
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error("Did not recive expected data");
+        const result = await response.json();
+        setNews(result);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    setTimeout(() => {
+      (async () => fetchItems())();
+    }, 2000);
+  }, []);
+  return <NewsCard newsSets={news}></NewsCard>;
 }
 
 export default App;
