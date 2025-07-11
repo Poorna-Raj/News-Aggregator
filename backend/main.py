60045a7f8a0293cded9e3fe5c83172ea6cc8db98
Poorna <poornarajapaksha339@gamil.com>
2025-07-12 01:49:41 +0530

    Add CORS middleware to FastAPI application for cross-origin requests

--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,17 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from typing import Union
 from fastapi import FastAPI
 from script.web_extract import scrape_news
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def get_news():
