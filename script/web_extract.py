b520709bc563deaf619ef0e7dfc749143917275b
Poorna <poornarajapaksha339@gamil.com>
2025-07-11 16:41:11 +0530

    Add web scraping functionality to extract news data and save to JSON

--- script/web_extract.py
+++ script/web_extract.py
@@ -1,3 +1,45 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+import pandas as pd
+
+# Container - //div[@class = "sc-cb78bbba-1 fYSNbR"]
+# Title - //div[@class = 'sc-666b6d83-0 jSTfiy"]//h2
+# Description - //div[@class = 'sc-666b6d83-0 jSTfiy']//p
+# Date - //div[@class = 'sc-666b6d83-0 jSTfiy']//span[@class = 'sc-ac6bc755-1 gxJSVz']
+# Category - //div[@class = 'sc-666b6d83-0 jSTfiy']//span[@class = 'sc-ac6bc755-2 ivCQgh']
+
+website = 'https://www.bbc.com/news'
+path = r'F:\Software\chromedriver-win64\chromedriver.exe'
+
+options = Options()
+
+service = Service(executable_path=path)
+driver = webdriver.Chrome(service=service)
+driver.get(website)
+
+titles = []
+descriptions = []
+dates = []
+categories = []
+
+containers = driver.find_elements(by='xpath',value='//div[@class = "sc-cb78bbba-1 fYSNbR"]')
+
+for container in containers:
+    try:
+        title = container.find_element(by='xpath',value='.//h2').text
+        description = container.find_element(by='xpath',value='.//p').text
+        date = container.find_element(by='xpath',value='.//span[@class = "sc-ac6bc755-1 gxJSVz"]').text
+        category = container.find_element(by='xpath',value='.//span[@class = "sc-ac6bc755-2 ivCQgh"]').text
+
+        titles.append(title)
+        descriptions.append(description)
+        dates.append(date)
+        categories.append(category)
+    except Exception as e:
+        print(e)
+
+dict_data = {'title':titles,'description':descriptions,'date':dates,'category':categories}
+df_news = pd.DataFrame(dict_data)
+df_news.to_json('news_data.json')
+driver.quit()        
